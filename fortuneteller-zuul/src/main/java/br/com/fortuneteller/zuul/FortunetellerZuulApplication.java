dffc6364f43171aec223cffb3ae6fa6385369c4d
djeffersonsilva <djeffersonsilva@vhsolucoes.com.br>
2020-04-21 20:14:45 -0300

    Configurações OAuth2 com o 'fortuneteller-zuul' e 'fortuneteller-predictor'

--- fortuneteller-zuul/src/main/java/br/com/fortuneteller/zuul/FortunetellerZuulApplication.java
+++ fortuneteller-zuul/src/main/java/br/com/fortuneteller/zuul/FortunetellerZuulApplication.java
@@ -1,12 +1,14 @@
-package br.com.fortuneteller;
+package br.com.fortuneteller.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableOAuth2Sso
 @SpringBootApplication
 public class FortunetellerZuulApplication {
 
