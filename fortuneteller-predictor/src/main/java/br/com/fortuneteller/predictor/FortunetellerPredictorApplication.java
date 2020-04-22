641791ccb199a99d35b70c8f4db6d15b771b76a0
djeffersonsilva <djeffersonsilva@vhsolucoes.com.br>
2020-04-21 21:03:36 -0300

    Configurações Resource server, e correções zuul + oauth

--- fortuneteller-predictor/src/main/java/br/com/fortuneteller/predictor/FortunetellerPredictorApplication.java
+++ fortuneteller-predictor/src/main/java/br/com/fortuneteller/predictor/FortunetellerPredictorApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableResourceServer
 @EntityScan(basePackages = "br.com.fortuneteller.predictor.domain.model")
 public class FortunetellerPredictorApplication {
 
