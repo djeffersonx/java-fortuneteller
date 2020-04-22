cbd38f2c82603a784f2e58cd0c6d15507be46214
djeffersonsilva <djeffersonsilva@vhsolucoes.com.br>
2020-04-21 21:30:56 -0300

    ...

--- fortuneteller-predictor/src/main/java/br/com/fortuneteller/predictor/application/PredictorController.java
+++ fortuneteller-predictor/src/main/java/br/com/fortuneteller/predictor/application/PredictorController.java
@@ -4,15 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.java.Log;
-
-@Log
 @RestController
 public class PredictorController {
 
 	@GetMapping("/predictor/predict/{item}")
 	public String predict(@PathVariable String item) {
-		log.warning("teste");
 		return item;
 	}
 
