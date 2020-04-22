212e57ea2cd6c4e6f9c96efe1152b7ecde890e5e
djeffersonsilva <djeffersonsilva@vhsolucoes.com.br>
2020-04-21 22:02:28 -0300

    ...

--- fortuneteller-zuul/src/main/java/br/com/fortuneteller/zuul/config/GatewayConfiguration.java
+++ fortuneteller-zuul/src/main/java/br/com/fortuneteller/zuul/config/GatewayConfiguration.java
@@ -2,11 +2,9 @@ package br.com.fortuneteller.zuul.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
-@EnableResourceServer
 public class GatewayConfiguration extends ResourceServerConfigurerAdapter {
 
 	@Override
