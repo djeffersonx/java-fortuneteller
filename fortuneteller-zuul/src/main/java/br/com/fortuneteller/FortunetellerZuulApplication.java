abcca32ae020473ccb7b6539a4eb8e171a887b11
djeffersonsilva <djeffersonsilva@vhsolucoes.com.br>
2020-04-21 21:37:51 -0300

    ...

--- fortuneteller-zuul/src/main/java/br/com/fortuneteller/FortunetellerZuulApplication.java
+++ fortuneteller-zuul/src/main/java/br/com/fortuneteller/FortunetellerZuulApplication.java
@@ -1,15 +1,17 @@
-package br.com.fortuneteller.predictor;
+package br.com.fortuneteller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@EnableZuulProxy
 @EnableDiscoveryClient
-public class FortunetellerPredictorApplication {
+@SpringBootApplication
+public class FortunetellerZuulApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FortunetellerPredictorApplication.class, args);
+		SpringApplication.run(FortunetellerZuulApplication.class, args);
 	}
 
 }
